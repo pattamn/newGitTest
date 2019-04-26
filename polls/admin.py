b925d1ce7a7ad0f51875f19c1aae2d40763bf35f
pattamn <pattamn@gmail.com>
2019-04-26 09:40:50 +0530

    Commiting manage.py

--- polls/admin.py
+++ polls/admin.py
@@ -4,4 +4,6 @@ from .models import  Question,Choice
 admin.site.register(Question)
 admin.site.register(Choice1)
 admin.site.register(Choice)
+admin.site.register(Choice2)
+admin.site.register(Choice1)
 # Register your models here.
