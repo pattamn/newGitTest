dd0c6fccf18548cb462451a8f5c4779f8f9d4ee8
pattamn <48521603+pattamn@users.noreply.github.com>
2019-04-26 09:39:33 +0530

    added choice1

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import  Question,Choice
 
 admin.site.register(Question)
-
+admin.site.register(Choice1)
 admin.site.register(Choice)
 # Register your models here.
